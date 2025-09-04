31607e0ffd9ef591fc3c03fbb777a65b24b4ac23
Nelson Sproul <nelson.sproul@gmail.com>
2025-09-04 12:21:25 -0700

    generate spreadsheets to detect metrics on m2

--- util/generate_property_template_spreadsheet.sh
+++ util/generate_property_template_spreadsheet.sh
@@ -39,49 +39,59 @@ Gen_csv()
 	echo ''
 }
 
-Append_new_fields_to_xlsx()
+Append_new_fields_to_csv()
 {
-	# Take the csv file and append the new fields to the xlsx file, extending the spreadsheet to the right.
-	if [ ! -r "$csv_fn" ]; then
-		echo "FAIL: expected readable file at \"$csv_fn\"" 1>&2
+	# Take the csv file and append the new fields to the csv file, extending the spreadsheet to the right.
+	if [ ! -r "$fields_to_add_csv_fn" ]; then
+		echo "FAIL: expected readable file at \"$fields_to_add_csv_fn\"" 1>&2
 		exit 1
 	fi
-	if [ ! -r "$input_xlsx_fn" ]; then
-		echo "FAIL: expected readable file at \"$input_xlsx_fn\"" 1>&2
+	if [ ! -r "$input_base_csv_fn" ]; then
+		echo "FAIL: expected readable file at \"$input_base_csv_fn\"" 1>&2
 		exit 1
 	fi
-	rm -f $output_xlsx_fn
-	if [ -f "$output_xlsx_fn" ]; then
-		echo "FAIL: file \"$output_xlsx_fn\" already exists" 1>&2
+	rm -f $output_csv_fn
+	if [ -f "$output_csv_fn" ]; then
+		echo "FAIL: file \"$output_csv_fn\" already exists" 1>&2
 		exit 1
 	fi
-	# Use add_worksheet1_metric.ts to append the new fields to the xlsx file.
-	ts_node=`pwd`/../node_modules/.bin/ts-node
-	if [ ! -f "$ts_node" ]; then
-		echo "FAIL: expected file at \"$ts_node\"" 1>&2
-		exit 1
-	fi
-	if $ts_node --project ../tsconfig.json src/add_worksheet1_metric.ts $input_xlsx_fn $csv_fn $output_xlsx_fn; then
-		echo "OK $ts_node --project ../tsconfig.json src/add_worksheet1_metric.ts $input_xlsx_fn $csv_fn $output_xlsx_fn" 1>&2
+        if csv_concat_right $input_base_csv_fn $fields_to_add_csv_fn > $output_csv_fn; then
+		echo "OK csv_concat_right $input_base_csv_fn $fields_to_add_csv_fn" 1>&2
 	else
-		echo "FAIL $ts_node --project ../tsconfig.json src/add_worksheet1_metric.ts $input_xlsx_fn $csv_fn $output_xlsx_fn" 1>&2
+		echo "FAIL csv_concat_right $input_base_csv_fn $fields_to_add_csv_fn" 1>&2
+                cat $output_csv_fn
                 exit 1
+        fi
+        if [ ! -f "$output_csv_fn" ]; then
+                echo "FAIL: expected file at \"$output_csv_fn\"" 1>&2
+                exit 1
+        else
+                echo "OK generated $output_csv_fn" 1>&2
         fi
 }
 
 set -o pipefail
 t=/tmp/$USER.generate_property_template_spreadsheet
-csv_fn=$t.csv
+fields_to_add_csv_fn=$t.csv
 excel_date_12_31_2024=45657
 debug_mode=''
 dry_mode=''
 QAFieldID0=''
 QAFieldIDn=''
-input_xlsx_fn=''
-output_xlsx_fn=''
+input_base_csv_fn=''
+output_csv_fn=''
 verbose_mode=''
 while [ -n "$1" ]; do
 	case "$1" in
+                -all)
+                        cd $dp/git/a/util/spreadsheets
+                        $0 -x -QAFieldID0 1 -QAFieldIDn 2	 -i y1.csv -o y3.csv
+                        $0 -x -QAFieldID0 1 -QAFieldIDn 20	 -i y1.csv -o y20.csv
+                        $0 -x -QAFieldID0 1 -QAFieldIDn 100	 -i y1.csv -o y100.csv
+                        $0 -x -QAFieldID0 1 -QAFieldIDn 500	 -i y1.csv -o y500.csv
+                        $0 -x -QAFieldID0 1 -QAFieldIDn 2000	 -i y1.csv -o y2000.csv
+                        $0 -x -QAFieldID0 1 -QAFieldIDn 5000	 -i y1.csv -o y5000.csv
+                ;;
 		-dry)
 			dry_mode=-dry
 		;;
@@ -93,13 +103,13 @@ while [ -n "$1" ]; do
 			shift
 			QAFieldIDn="$1"
 		;;
-		-i|-input_xlsx_fn)
+		-i|-input_base_csv_fn)
 			shift
-			input_xlsx_fn="$1"
+			input_base_csv_fn="$1"
 		;;
-		-o|-output_xlsx_fn)
+		-o|-output_csv_fn)
 			shift
-			output_xlsx_fn="$1"
+			output_csv_fn="$1"
 		;;
 		-q|-quiet)
 			verbose_mode=''
@@ -121,10 +131,10 @@ while [ -n "$1" ]; do
 	esac
 	shift
 done
-Gen_csv > $csv_fn
-Append_new_fields_to_xlsx
+Gen_csv > $fields_to_add_csv_fn
+Append_new_fields_to_csv
 
 exit
-$dp/git/a/util/generate_property_template_spreadsheet.sh -QAFieldID0 1 -QAFieldIDn 20 -i $dp/git/a/util/spreadsheets/z.xlsx -o $dp/git/a/util/spreadsheets/z20.xlsx
+$dp/git/a/util/generate_property_template_spreadsheet.sh -QAFieldID0 1 -QAFieldIDn 20 -i $dp/git/a/util/spreadsheets/z.csv -o $dp/git/a/util/spreadsheets/z20.csv
 exit
-$dp/git/a/util/generate_property_template_spreadsheet.sh -x -QAFieldID0 1 -QAFieldIDn 2	 -i $dp/git/a/util/spreadsheets/z.xlsx -o $dp/git/a/util/spreadsheets/z2.xlsx
+$dp/git/a/util/generate_property_template_spreadsheet.sh -x -QAFieldID0 1 -QAFieldIDn 2	 -i $dp/git/a/util/spreadsheets/y1.csv -o $dp/git/a/util/spreadsheets/z3.csv
